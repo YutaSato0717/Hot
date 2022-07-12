e4cda00338d1117c1fbbcfc898f844b7f6f126c6
YutaSato0717 <s1290178@sshsv211.u-aizu.ac.jp>
2022-07-12 14:41:40 +0900

    Victory message added

--- HoT.c
+++ HoT.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 void HoT(void);
 int countH=0,countT=0;
 int main(){
   printf("Tpssing a coin...\n");
+  srand(time(NULL));
   for(int i=1;i<4;i++){
     printf("Round %d: ",i);
     HoT();
     printf("\n");
   }
   printf("Heads: %d, Tails: %d\n",countH,countT);
+  if(countH>countT)printf("You won!\n");
+  else if(countH<countT)printf("You lost!\n");
   return 0;
 }
 
